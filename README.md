470ee5350cb2845891f3f629fad0bfb6e1970f50
pafable <philafable@gmail.com>
2024-01-03 21:56:25 -0500

    test bot message

---
+++ README.md
@@ -0,0 +1,3 @@
+# DISCORD BOT 2
+
+This project will be converting my Java based discord bot to use Go.
--- main.go
+++ main.go
@@ -1,24 +1,44 @@
 package main
 
 import (
-	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-	discord, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	// Creates discord session
+	sess, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println(*discord)
-	var inputVar string
-	scan, err := fmt.Scan(&inputVar)
+	// processes incoming messages from discord
+	sess.AddHandler(
+		func(s *discordgo.Session, m *discordgo.MessageCreate) {
+			if m.Author.ID == s.State.User.ID {
+				return
+			}
+
+			if m.Content == "hello" {
+				s.ChannelMessageSend(m.ChannelID, "world!")
+			}
+		})
+
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	err = sess.Open()
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 
-	log.Println(scan, inputVar)
+	defer sess.Close()
+
+	log.Println("The bot is online")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
 }
